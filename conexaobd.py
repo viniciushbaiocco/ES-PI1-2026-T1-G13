5e0db8e82361ed3aee4c8614257a0a009a8034ad
Vinicius Hegues Baiocco <vinicius.baiocco@gmail.com>
2026-04-14 17:04:50 -0300

    Testada a conexão por via WSL e Windows, certificada a funcionalidade, versão final

--- conexaobd.py
+++ conexaobd.py
@@ -1,1 +1,23 @@
+import mysql.connector
 
+
+def conectar_banco():
+    conexao = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Vinicius@1243",
+        database="lad_py"
+    )
+    return conexao
+
+def testar_conexao():
+    try:
+        conexao = conectar_banco()
+        if conexao.is_connected():
+            print("Conexao com o banco realizada com sucesso.")
+        conexao.close()
+    except mysql.connector.Error as erro:
+        print(erro)
+    return conexao
+
+testar_conexao()
