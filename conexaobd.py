4ffa1d6cdd19e818fd19d3ac34cea472d60f6b97
LUCAS DA MATA PRADO <lucas.mp7@puccampinas.edu.br>
2026-04-16 19:36:34 -0300

    Adiciona estrutura inicial do cadastro de eleitor com integração ao banco

--- conexaobd.py
+++ conexaobd.py
@@ -15,3 +15,14 @@ def testar_conexao():
 
     except mysql.connector.Error as erro:
         print("Erro ao conectar com o banco:", erro)
+def executar(comando,valores):
+       conexao = mysql.connector.connect(
+            host="localhost", #sua hospedagem de rede (localhost ou 127.0.0.1 por padrão)
+            user="root", #seu usuario no mysql server (root é o padrão por maquina, se nunca utilizou mysql server antes, deixe do jeito que está)
+            password="senha", #sua senha
+            database="lad_py" #não alterar, nome da database de banco.sql
+        )
+       cursor=conexao.cursor()
+       cursor.execute(comando,valores)
+       conexao.commit
+       cursor.close
